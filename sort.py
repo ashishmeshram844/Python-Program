0803d99d31b3e9cea64aad4603b4fb4e71aa4f21
Ashish Meshram <112160710+ashishmeshram844@users.noreply.github.com>
2022-10-27 14:35:31 +0530

    Sorting list python program
    This file contain sorting a list in ascending and descending order program

---
+++ sort.py
@@ -0,0 +1,50 @@
+""" REMOVE DUPLICATES FROM LIST AND SORT IN ASCENDING AND 
+    DESCENDING ORDER WITHOUT USING INBUILT FUNCTIONS """
+
+def input_list():
+    """ 
+    This function takes size of list and input the values 
+    as size is inputed
+    - returns -> list
+    """
+    size = int(input("Enter size of list : "))
+    return list(int(input()) for i in range(size))
+    
+def get_unique_list(lst):
+    """ 
+    This function  return list of ubique elements
+    - returns -> list
+    """
+    return list(set(lst))
+    
+def sort_list_in_ascending(lst):
+    """ 
+    This function sort the list in ascending order
+    - Arguments : 
+        - lst -> list of elements which we want to sort
+    - returns:
+        - lst -> sorted list
+    """
+    for i in range(len(lst)):
+        def swap_element_in_list(j):
+             if lst[i] < lst[j]:
+                lst[i],lst[j] = lst[j],lst[i]
+        x = map(swap_element_in_list,range(len(lst)))
+    return lst
+
+
+  
+def sort_list_in_descending(lst):
+    """ 
+    This function sort the list in descending order
+    - Arguments : 
+        - lst -> list of elements which we want to sort
+    - returns:
+        - lst -> sorted list
+    """
+    return lst[::-1]
+
+#programme execution starts from here
+lst = input_list()
+print(sort_list_in_ascending(get_unique_list(lst)))
+print(sort_list_in_descending(sort_list_in_ascending(get_unique_list(lst))))
