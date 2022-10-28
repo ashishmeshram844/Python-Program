8798ded3c5b744ad21cbe2b4a6c830f7cde69685
Ashish Meshram <112160710+ashishmeshram844@users.noreply.github.com>
2022-10-28 14:46:18 +0530

    recirsive function added

--- fibonacci.py
+++ fibonacci.py
@@ -20,14 +20,32 @@ def generate_fibonacci_series(num:int):
     """
     fib = [0,1]
     if num > 2:
-        for i in range(1,num):
+        for i in range(1,num-1):
             fib.append(fib[-1]+fib[-2])
         return fib
     return "Make sure input is greate than 2 "
 
+def recursive_fibonacci(num,fibs):
+    """ 
+    This function gives the fibonacci series 
+    - Arguments : 
+        - num : number we need fibonacci series
+        - fibs : fibonacci series
+    - returns : 
+        - fibs : final fibonacci series 
+    """
+    if len(fibs) >= num:
+        return fibs
+    else:
+        fibs.append(fibs[-1]+fibs[-2])
+        return recursive_fibonacci(num,fibs)
+
 def main():
     num = input_number()
     fibonacci_list = generate_fibonacci_series(num)
+    print(fibonacci_list)
+    fibs = [0,1]
+    fibonacci_list = recursive_fibonacci(num,fibs)
     print(fibonacci_list)
 
 if __name__ == "__main__":
