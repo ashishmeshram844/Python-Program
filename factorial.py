f9646a11848181e3aff9a05995a4a72afd76e46f
Ashish Meshram <112160710+ashishmeshram844@users.noreply.github.com>
2022-10-28 11:20:10 +0530

    factorial of a number

---
+++ factorial.py
@@ -0,0 +1,34 @@
+""" 
+WRITE A PROGRAM TO FIND FACTORIAL OF A NUMBER
+"""
+def input_number():
+    """ 
+    This function takes the input from user 
+    - return :
+        - inputed number (int)
+    """
+    return int(input("Enter Number to find Factorial : "))
+
+def factorial(num):
+    """ 
+    This function find the factorial of a given number
+    - Argument:
+        - num : int
+    return :
+        - fact : factorial of a number 
+    """
+    fact = 1
+    for i in range(1,num+1):
+        fact = fact*i
+    return fact
+
+def main():
+    num = input_number()
+    factorial_of_num = factorial(num)
+    print(factorial_of_num)
+
+if __name__ == "__main__":
+    """ 
+    Execution of program starts from here
+    """
+    main()
