04e70fcd4c45e3535acc6a717f70177a9640ddfa
Ashish Meshram <112160710+ashishmeshram844@users.noreply.github.com>
2022-10-28 14:25:17 +0530

    fibonnaci series  upto given number

---
+++ fibonacci.py
@@ -0,0 +1,38 @@
+""" 
+write a program to print fibonacci series upto given number
+"""
+
+def input_number():
+    """ 
+    This function takes the input from user 
+    - return :
+        - inputed number (int)
+    """
+    return int(input("Enter Number to find Factorial : "))
+
+def generate_fibonacci_series(num:int):
+    """ 
+    This function returns the fibonacci series upto the given number
+    - Arguments :
+        num : int
+    - return : 
+        - fib : list
+    """
+    fib = [0,1]
+    if num > 2:
+        for i in range(1,num):
+            fib.append(fib[-1]+fib[-2])
+        return fib
+    return fib
+
+
+def main():
+    num = input_number()
+    fibonacci_list = generate_fibonacci_series(num)
+    print(fibonacci_list)
+
+if __name__ == "__main__":
+    """ 
+    Execution of program starts from here
+    """
+    main()
