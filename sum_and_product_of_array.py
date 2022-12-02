caf0339f9b3661a9d57d63ea72055db4d198a967
Ashish Meshram <112160710+ashishmeshram844@users.noreply.github.com>
2022-12-02 12:01:47 +0530

    Create sum_and_product_of_array.py

---
+++ sum_and_product_of_array.py
@@ -0,0 +1,28 @@
+def find_sum_and_product(array):
+    """ 
+    - This function  list of numbers and find the sum and product of all
+    this elements.
+    - Argument : 
+        - array : list 
+    returns : 
+        - sum : int (sum of elements presenet in array)
+        - product : int (product of elements present in array)
+    """
+    sum,product = 0,1
+    for num in array:
+        sum += num
+    for num in array:
+        product *= num
+    return sum,product
+
+if __name__ == "__main__":
+    """ 
+    - This is main mfunction execution starts from here
+    - generate array between range 1 to 12 with diffrence of 3
+    """
+    array = [i for i in range(1,12,3)]
+    sum,product = find_sum_and_product(array)
+    print("The Array is : ",array)
+    print(f"SUM = {sum} and PRODUCT = {product}")
+
+
