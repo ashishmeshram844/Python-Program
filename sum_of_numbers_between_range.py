46111b84c4d458262c75c019eae1bbfc1bdd9e79
Ashish Meshram <112160710+ashishmeshram844@users.noreply.github.com>
2023-09-28 15:12:23 +0530

    Create sum_of_numbers_between_range

---
+++ sum_of_numbers_between_range.py
@@ -0,0 +1,43 @@
+"""
+Find Sum of Numbers  within given range
+"""
+
+class CalculateSum:
+    """ This class contain all operations for calculating the sum of number 
+        betweeen the given range
+    """
+    def __init__(self):
+        pass
+    def takeInputs(self):
+        """ This function takes the starting and ending range values from user"""
+        try:
+            self.start_val = int(input("Enter Starting : "))
+            self.end_val = int(input("Enter End : "))
+            return True
+        except ValueError: 
+            print("Integer Required ... ")
+            return False
+        else:
+            return False
+    
+    def printInputs(self):
+        """ This function simply shows text that our calculation is in process """
+        print(f"calculating sum from {self.start_val } to {self.end_val} ...")
+        
+    def calculation(self,end_val):
+        """ This function calculate the sum of given range recursively """
+        if end_val < self.start_val:
+            return 0
+        else:
+            return end_val + self.calculation(end_val -1)
+       
+       
+if __name__ == "__main__":
+    """ Main mehod Execution Starts from here """
+    obj = CalculateSum()
+    if obj.takeInputs():
+        obj.printInputs()
+        print("Answer is : ",obj.calculation(obj.end_val))
+        
+        
+                
